c60096b606492d2d6b9612d6271bc3a45040d780
jcapellom <jcapellom@gmail.com>
2022-06-23 10:07:47 -0300

    sigwx

--- SkyBot.js
+++ SkyBot.js
@@ -14,34 +14,43 @@ bot.start(ctx => {
 
 bot.help(ctx => {
     const textHelp = '*/metar* Utilize este comando para obter o METAR das localidades solicitadas, enumerando\\-as com espaço após o comando e separando\\-as com vírgula\\.\n_Ex: /metar sbrj,sbgl,sbjr_\n\
-\n*/taf*'
+\n*/taf* Utilize este comando para obter o TAF das localidades solicitadas, enumerando\\-as com espaço após o comando e separando\\-as com vírgula\\.\n_Ex: /taf sbrj,sbgl,sbjr_\n\
+\n*/sigwx*  Utilize este comando para obter a última carta SIGWX baixa disponível \\(SUP/FL250\\)\\.\n_Ex: /sigwx_' 
     ctx.telegram.sendMessage(ctx.chat.id, textHelp, {parse_mode: 'MarkdownV2'});
 });
 
-bot.command([customCommands.metar, customCommands.notam, customCommands.taf], (ctx) => {
-    let text = ctx.update.message.text;
-    let command = text.split(' ')[0].split('/')[1];
-    let returnMessage = handleCommandMessage(text);
-    if (returnMessage.reply != '') {
-        ctx.reply(returnMessage.reply);
-        return;
-    }
-    let requestedLocations = returnMessage.handledLocations;
+bot.command([customCommands.metar, customCommands.notam, customCommands.taf, customCommands.sigwx], (ctx) => {
+    let command = extractCommandFromMessage(ctx.update.message.text);
     switch (command.toUpperCase()) {
+        case customCommands.notam.toUpperCase():
         case customCommands.metar.toUpperCase():
         case customCommands.taf.toUpperCase():
-            ctx.reply(`Buscando ${command.toUpperCase()} para as localidades ${requestedLocations}`);
-            redeMetApi.getMetarOrTaf(command, requestedLocations, '').then(res =>{
+            let returnMessage = handleCommandMessage(ctx.update.message.text);
+            let requestedLocations = returnMessage.handledLocations;
+            if (returnMessage.reply != '') {
+                ctx.reply(returnMessage.reply);
+                return;
+            }
+            ctx.reply(`Buscando ${command.toUpperCase()} para as localidades ${requestedLocations}...`);
+            redeMetApi.getMetarOrTaf(command, requestedLocations, '').then(res => {
                 ctx.reply(res);
             });
             break;
-        case customCommands.notam.toUpperCase():
-
+        case customCommands.sigwx.toUpperCase():
+            ctx.reply(`Buscando ${command.toUpperCase()} mais recente...`);
+            redeMetApi.getSigwx().then(res => {
+                console.log(res);
+                ctx.replyWithPhoto(res);
+            })
         default:
             break;
     }
 
 });
+
+function extractCommandFromMessage(text) {
+    return text.split(' ')[0].split('/')[1];
+}
 
 function handleLocations(locations){
     return locations !== undefined ? locations.split(',').map(location => location.toUpperCase()) : 0;
--- redeMetApi.js
+++ redeMetApi.js
@@ -1,59 +1,51 @@
 const redeMetApiKey = 'OM55qUBbwLma4RYXBf1QDYRPkIEyQD99KJ6fs94L';
-const baseUrl = 'https://api-redemet.decea.mil.br/mensagens/';
+const baseUrlMensagens = 'https://api-redemet.decea.mil.br/mensagens/';
+const baseUrlProdutos = 'https://api-redemet.decea.mil.br/produtos/';
 const util = require('./util');
 const axios = require('axios');
 
 module.exports = {
 
     redeMetApiKey,
-
-    baseUrl,
-
     getMetarOrTaf,
-
-    sigmet: function (pais, data_ini, data_fim, page_tam) {
-        let endpoint = 'sigmet/';
-        return `${baseUrl}${endpoint}?api_key=${redeMetApiKey}&data_ini=${data_ini}&data_fim=${data_fim}&page_tam=${page_tam}&pais=${pais}`;
-    },
-
-    taf: function (localidades, data_ini, data_fim, page_tam, quebra_linha) {
-        let endpoint = 'taf/';
-        return `${baseUrl}${endpoint}${localidades}?api_key=${redeMetApiKey}&data_ini=${data_ini}&data_fim=${data_fim}&page_tam=${page_tam}&fim_linha=${quebra_linha}`;
-    },
-
-    sigwx: function () {
-        let endpoint = 'sigwx/';
-        return `${baseUrl}${endpoint}?api_key=${redeMetApiKey}`;
-    }
+    getSigwx
 
 };
 
 function getMetarOrTaf(metarOrTaf, requestedLocations, finalMessage) {
     let endpoint = metarOrTaf + '/';
-    let response
-    let data_ini
-    let data_fim
-    let page_tam
+    let response;
+    let data_ini;
+    let data_fim;
+    let page_tam;
     return new Promise((resolve) => {
-        let requestURL = `${baseUrl}${endpoint}${requestedLocations}?api_key=${redeMetApiKey}`;
-        axios.get(requestURL).then(res => {
+        let requestUrl = `${baseUrlMensagens}${endpoint}${requestedLocations}?api_key=${redeMetApiKey}`;
+        axios.get(requestUrl).then(res => {
             response = res.data !== undefined ? res.data.data !== undefined ? res.data.data.data : 0 : 0;
             let returnedMessages = [];
             let foundLocations = [];
             if (response == 0) {
                 finalMessage = `Não há ${metarOrTaf.toUpperCase()} disponível para nenhuma localidade requisitada.`;
-                resolve(finalMessage)
+                resolve(finalMessage);
             }
             else
                 response.forEach((item) => {
-                    returnedMessages.push({ location: item.id_localidade, initial: item.validade_inicial, final: item.validade_final, msg: item.mens, received: item.recebimento })
-                    foundLocations.push(item.id_localidade)
+                    returnedMessages.push({ location: item.id_localidade, initial: item.validade_inicial, final: item.validade_final, msg: item.mens, received: item.recebimento });
+                    foundLocations.push(item.id_localidade);
                 });
             let notFoundLocations = util.arrayDifference(requestedLocations, foundLocations);
-            returnedMessages.forEach(item => finalMessage += (item.msg + '\n' + '\n'))
+            returnedMessages.forEach(item => finalMessage += (item.msg + '\n' + '\n'));
             if (notFoundLocations != 0) finalMessage += `Não há ${metarOrTaf.toUpperCase()} disponível para ${notFoundLocations}`;
-            resolve(finalMessage)
+            resolve(finalMessage);
         });
     });
 }
 
+function getSigwx(){
+    return new Promise((resolve) => {
+        let requestUrl = `${baseUrlProdutos}sigwx?api_key=${redeMetApiKey}`;
+        axios.get(requestUrl).then(res => {
+            resolve(res.data);
+        })
+    })
+}
