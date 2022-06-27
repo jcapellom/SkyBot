e71e9599be4e2bc834d0e01abf50ed06d487c191
jcapellom <jcapellom@gmail.com>
2022-06-27 17:22:49 -0300

    util export fix

--- util.js
+++ util.js
@@ -1,9 +1,4 @@
-module.exports = {
-    arrayDifference,
-    toISOStringWithTimezone
-}
-
-function arrayDifference(arr1, arr2){
+function arrayDifference(arr1, arr2) {
     return Array.isArray(arr1) && Array.isArray(arr2) ? arr1.filter(x => !arr2.includes(x)) : 0;
 };
 
@@ -12,11 +7,16 @@ const toISOStringWithTimezone = date => {
     const diff = tzOffset >= 0 ? '+' : '-';
     const pad = n => `${Math.floor(Math.abs(n))}`.padStart(2, '0');
     return date.getFullYear() +
-      '-' + pad(date.getMonth() + 1) +
-      '-' + pad(date.getDate()) +
-      'T' + pad(date.getHours()) +
-      ':' + pad(date.getMinutes()) +
-      ':' + pad(date.getSeconds()) +
-      diff + pad(tzOffset / 60) +
-      ':' + pad(tzOffset % 60);
-  }
+        '-' + pad(date.getMonth() + 1) +
+        '-' + pad(date.getDate()) +
+        'T' + pad(date.getHours()) +
+        ':' + pad(date.getMinutes()) +
+        ':' + pad(date.getSeconds()) +
+        diff + pad(tzOffset / 60) +
+        ':' + pad(tzOffset % 60);
+}
+
+module.exports = {
+    arrayDifference,
+    toISOStringWithTimezone
+}
